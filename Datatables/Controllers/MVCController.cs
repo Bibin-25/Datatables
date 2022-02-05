41d26fef7d9ca59e961ef4f9ce82f4452045259f
Bibin <bibinscaria25@gmail.com>
2022-02-05 00:21:47 -0500

    Add project files.

---
+++ Datatables/Controllers/MVCController.cs
@@ -0,0 +1,24 @@
+﻿using Datatables.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Datatables.Controllers
+{
+    public class MVCController : Controller
+    {
+        // GET: MVC
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            DataTableEntities db = new DataTableEntities();
+            var data = db.mvcs.OrderBy(a => a.Name).ToList();
+            return Json(new {data=data},JsonRequestBehavior.AllowGet);
+        }
+    }
+}
